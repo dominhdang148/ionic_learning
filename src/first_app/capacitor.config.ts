8125df4fa995bfd5ff1fc116b714dee62c916fda
dominhdang148 <myclone235@gmail.com>
2023-09-14 22:44:23 +0700

    Finish lesson 2

--- src/first_app/capacitor.config.ts
+++ src/first_app/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'first-app',
+  appName: 'first_app',
   webDir: 'www',
   server: {
     androidScheme: 'https'
